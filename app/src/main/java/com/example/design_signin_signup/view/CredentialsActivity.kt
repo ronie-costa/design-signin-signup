5ad0d8f1347189b1f1a8e9dd17786a2400a7577e
Ronie Costa <rone8546@gmail.com>
2022-02-17 02:33:37 -0300

    Crianção do ViewPager da CredentialsActivity

--- app/src/main/java/com/example/design_signin_signup/view/CredentialsActivity.kt
+++ app/src/main/java/com/example/design_signin_signup/view/CredentialsActivity.kt
@@ -3,13 +3,19 @@ package com.example.design_signin_signup.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
 import com.example.design_signin_signup.databinding.ActivityCredentialsBinding
+import com.example.design_signin_signup.services.adapter.CredentialsFragmentAdapter
+import com.example.design_signin_signup.view.fragment.InitialFragment
+import com.example.design_signin_signup.view.fragment.SignInFragment
+import com.example.design_signin_signup.view.fragment.SignUpFragment
 import com.example.design_signin_signup.viewmodel.CredentialsViewModel
 
 class CredentialsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCredentialsBinding
     private lateinit var viewModel: CredentialsViewModel
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +25,25 @@ class CredentialsActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this)[CredentialsViewModel::class.java]
 
+        val listFragments = mutableListOf(
+            SignInFragment(),
+            InitialFragment(),
+            SignUpFragment()
+        )
+
+        val adapterFragment = CredentialsFragmentAdapter(
+            supportFragmentManager,
+            lifecycle,
+            listFragments
+        )
+        viewPager = binding.viewPagerCredentials
+        viewPager.adapter = adapterFragment
+        viewPager.setCurrentItem(1, false)
+
+        observers()
+    }
+
+    private fun observers() {
 
     }
 }
